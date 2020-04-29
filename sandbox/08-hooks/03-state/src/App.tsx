f0e4345149a1646b5f093209aa4c6812c488e816
neotag <harada@gaji.jp>
2020-04-29 15:29:34 +0900

    関数コンポーネントに書き換え

--- sandbox/08-hooks/03-state/src/App.tsx
+++ sandbox/08-hooks/03-state/src/App.tsx
@@ -1,57 +1,49 @@
-import React, { Component } from 'react';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { FC } from 'react';
 import { Button, Card, Statistic } from 'semantic-ui-react';
 
 import './App.css';
 
-interface AppState {
-  count: number;
-}
+const App: FC = () => {
+  const count = 0;
+  const increment = () => {};
+  const decrement = () => {};
 
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { count: 0 };
-  }
+  //  increment() {
+  //  this.setState(prevState => ({
+  //    count: prevState.count + 1,
+  //  }));
+  //  }
 
-  increment() {
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }));
-  }
+  //  decrement() {
+  //  this.setState(prevState => ({
+  //    count: prevState.count - 1,
+  //  }));
+  //  }
 
-  decrement() {
-    this.setState(prevState => ({
-      count: prevState.count - 1,
-    }));
-  }
-
-  render() {
-    const { count } = this.state;
-
-    return (
-      <div className="container">
-        <header>
-          <h1>カウンター</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board">
-            <Statistic.Label>count</Statistic.Label>
-            <Statistic.Value>{count}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <div className="ui two buttons">
-              <Button color="red" onClick={() => this.decrement()}>
-                -1
-              </Button>
-              <Button color="green" onClick={() => this.increment()}>
-                +1
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <header>
+        <h1>カウンター</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui two buttons">
+            <Button color="red" onClick={decrement}>
+              -1
+            </Button>
+            <Button color="green" onClick={increment}>
+              +1
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default App;
