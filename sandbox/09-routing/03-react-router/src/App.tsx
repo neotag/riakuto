b7232bc880e83084970e4dcc8696e8f964de540e
neotag <harada@gaji.jp>
2020-05-02 16:01:39 +0900

    Add School data

--- sandbox/09-routing/03-react-router/src/App.tsx
+++ sandbox/09-routing/03-react-router/src/App.tsx
@@ -1,39 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import CharacterList, { Character } from './CharacterList';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    const characters: Character[] = [
-      {
-        id: 1,
-        name: '羽咲 綾乃',
-        age: 16,
-        height: 151,
-      },
-      {
-        id: 2,
-        name: '荒垣 なぎさ',
-        age: 18,
-        height: 174,
-      },
-      {
-        id: 3,
-        name: '泉 理子',
-        age: 18,
-      },
-    ];
+import { schoolData } from './data';
 
-    return (
-      <div className="container">
-        <header>
-          <h1>はねバド！ キャラクター一覧</h1>
-        </header>
-        <CharacterList school="北小町高校" characters={characters} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.dir(schoolData);
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>はねバド！ キャラクター一覧</h1>
+      </header>
+      <CharacterList
+        school="北小町高校"
+        characters={schoolData.kitakomachi.students}
+      />
+    </div>
+  );
+};
 
 export default App;
