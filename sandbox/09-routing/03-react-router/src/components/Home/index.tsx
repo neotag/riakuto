1efec49b655418de26fb0a2751ceb90273d9e61a
neotag <harada@gaji.jp>
2020-05-02 16:59:04 +0900

    react-router で遷移つくったりした

--- sandbox/09-routing/03-react-router/src/components/Home/index.tsx
+++ sandbox/09-routing/03-react-router/src/components/Home/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import CharacterList from '../../CharacterList';
 
 import '../../App.css';
@@ -6,11 +7,14 @@ import '../../App.css';
 import { schoolData } from '../../data';
 
 const Home: FC<{}> = () => {
+  const code = 'kitakomachi';
+
   return (
     <div className="container">
       <header>
         <h1>はねバド！ キャラクター一覧</h1>
       </header>
+      <Link to={`/temp/${code}`}>temp</Link>
       <CharacterList
         school="北小町高校"
         characters={schoolData.kitakomachi.students}
--- sandbox/09-routing/03-react-router/src/components/Temp/index.tsx
+++ sandbox/09-routing/03-react-router/src/components/Temp/index.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import { parse } from 'query-string';
 
-const Home: FC<{}> = () => {
+type TempProps = {} & RouteComponentProps<{ code: string }>;
+
+const Temp: FC<TempProps> = ({ location, match }) => {
+  const { code } = match.params;
+  const { s } = parse(location.search);
+  // eslint-disable-next-line no-console
+  console.log(code);
+
   return (
     <div className="container">
       <Helmet>
@@ -9,11 +19,13 @@ const Home: FC<{}> = () => {
       </Helmet>
 
       <header>
-        <h1>React Router sandbox - Temporary page</h1>
+        <h1>React Router sandbox - {code} page</h1>
+        {s ? `query: ${s}` : ''}
       </header>
       <p>ただのサンプルぺーじ。</p>
+      <Link to="/">とっぷぺーじへもどる</Link>
     </div>
   );
 };
 
-export default Home;
+export default withRouter(Temp);
