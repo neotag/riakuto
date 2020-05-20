42170383899322aa49b7ac11d9aab238a3e1b3d4
neotag <harada@gaji.jp>
2020-05-20 23:04:01 +0900

    Redux saga した

--- sandbox/11-async/03-saga/src/App.tsx
+++ sandbox/11-async/03-saga/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import { Helmet } from 'react-helmet';
+import { Redirect, Route, Switch } from 'react-router';
+import Home from './components/Home';
+import Members from './containers/Members';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const title = 'いろんな会社のメンバー';
+
+const App: FC = () => (
+  <>
+    <Helmet htmlAttributes={{ lang: 'ja' }}>
+      <title>{title}</title>
+    </Helmet>
+
+    <header className="App-header">
+      <h1>{title}</h1>
+    </header>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/:companyName/members" component={Members} />
+      <Redirect to="/" />
+    </Switch>
+  </>
+);
 
 export default App;
